ced845a9b2391274e705d34bdf8c4b927e64c98a
i-webtest <stasbass80@gmail.com>
2025-04-16 22:35:19 +0300

    Add eslint, PropTypes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import style from './Layout.module.css';
+import PropTypes from 'prop-types';
 
+// eslint-disable-next-line arrow-body-style
 export const Layout = ({ className, children }) => {
   return className ? (
     <div className={`${style.container} ${className}`}>{children}</div>
   ) : (
     <div className={style.container}>{children}</div>
   );
+};
+
+Layout.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Main.module.css';
 import { Layout } from '../Layout/Layout';
 
+// eslint-disable-next-line arrow-body-style
 export const Main = () => {
   return (
     <main className={style.main}>
